b1337de03684b93bc3c8b2502ba053d8a5fbc26d
oiji-official <xyz.notimportant@gmail.com>
2021-04-14 22:34:11 +0530

    debug port

--- main.py
+++ main.py
@@ -282,7 +282,8 @@ def delete_post(post_id):
     return redirect(url_for('blog_page'))
 
 
-port = int(os.environ.get('PORT', 5000))
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
 
