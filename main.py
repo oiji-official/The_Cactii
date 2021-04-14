8aeab0b55c2057cd7a2ebc6534cd34530252204a
oiji-official <xyz.notimportant@gmail.com>
2021-04-14 22:36:59 +0530

    0.0.0.0 to localhost

--- main.py
+++ main.py
@@ -285,5 +285,5 @@ def delete_post(post_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='localhost', port=port, debug=True)
 
