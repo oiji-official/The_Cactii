fed923dbd2453771604bd185f1da93219e5391c7
oiji-official <xyz.notimportant@gmail.com>
2021-04-14 13:34:52 +0530

    debug SECRET KEY

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ OWN_PASSWORD = os.environ.get("OWN_PASSWORD")
 #'sqlite:///data.db'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
